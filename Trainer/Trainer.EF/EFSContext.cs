81555f550165cee7520e8136fae2669fe2dd38fd
IT86LDR\ahmed.kabbash <ahmedkabbash@gmail.com>
2018-11-25 01:36:21 +0200

    expose port 80

--- Trainer/Trainer.EF/EFSContext.cs
+++ Trainer/Trainer.EF/EFSContext.cs
@@ -14,7 +14,7 @@ namespace Trainer.EF
         public EFSContext(DbContextOptions<EFSContext> options)
             : base(options)
         {
-            Database.EnsureCreated();
+            // Database.EnsureCreated();
             Database.Migrate();
         }
 
