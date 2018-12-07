ef4b5c1b05d7661a04970181852616ad61688305
IT86LDR\ahmed.kabbash <ahmedkabbash@gmail.com>
2018-12-08 01:04:33 +0200

    adding resolvers

--- Trainer-FrontEnd/src/environments/environment.prod.ts
+++ Trainer-FrontEnd/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'http://ec2-54-245-218-55.us-west-2.compute.amazonaws.com:4400/api/',
-  filesBaseUrl : 'http://ec2-54-245-218-55.us-west-2.compute.amazonaws.com:4400'
+  filesBaseUrl : 'http://ec2-54-245-218-55.us-west-2.compute.amazonaws.com:4400/'
 };
