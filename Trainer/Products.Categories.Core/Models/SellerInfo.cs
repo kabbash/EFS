3fe0699b88fb4274e9f01e7aa1d2c7e0e9ed9045
ahmed <ahmedkabbash@gmail.com>
2018-12-09 22:48:21 +0200

    adding seller info to product

---
+++ Trainer/Products.Categories.Core/Models/SellerInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Products.Core.Models
+{
+    public class SellerInfo
+    {
+        public string Name { get; set; }
+        public string Phones { get; set; }
+    }
+}
