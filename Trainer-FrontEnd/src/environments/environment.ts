eb3b128521a4bbaa4625ad9040cd9d68a2767896
mustafa ahmed <mustafa.ahmed@algorithmz.local>
2019-01-10 00:49:45 +0200

    edit-article-details-slider

--- Trainer-FrontEnd/src/environments/environment.ts
+++ Trainer-FrontEnd/src/environments/environment.ts
@@ -2,17 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   baseUrl: 'https://localhost:44394/api/',
-//   filesBaseUrl : 'https://localhost:44394/'
-// };
-
 export const environment = {
   production: false,
-  baseUrl: 'http://ec2-34-220-10-104.us-west-2.compute.amazonaws.com:4400/api/',
-  filesBaseUrl : 'http://ec2-34-220-10-104.us-west-2.compute.amazonaws.com:4400/'
+  baseUrl: 'https://localhost:44394/api/',
+  filesBaseUrl: 'https://localhost:44394/'
 };
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
