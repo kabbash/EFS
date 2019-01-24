2e6134861725e647a561a1941e6462dcacfc4e77
mohamedm.magdi91 <mohamedm.magdi91@gmail.com>
2019-01-24 20:04:01 +0200

    slider

---
+++ Trainer/AttachmentsManager/Interfaces/ISliderManager.cs
@@ -0,0 +1,16 @@
+﻿using Attachments.Core.Models;
+using Shared.Core.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attachments.Core.Interfaces
+{
+    public interface ISliderManager<T> where T : IImageBase
+    {
+        bool Add(SliderDto dto);
+        bool Update(SliderDto dto);
+        bool Delete(SliderDto dto);
+        string GetProfilePicturePath(SliderDto dto);
+    }
+}
--- Trainer/Shared.Core/Models/ClientsImages.cs
+++ Trainer/Shared.Core/Models/ClientsImages.cs
@@ -5,13 +5,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Shared.Core.Models
 {
-    public partial class ClientsImages :IImageBase
+    public partial class ClientsImages 
     {
         public int Id { get; set; }
         [Required]
         public string Path { get; set; }
         public string Title { get; set; }
-        public string Text { get; set; }
         public DateTime Date { get; set; }
         public string ClientId { get; set; }
         public DateTime CreatedAt { get; set; }
