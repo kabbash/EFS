e59070f8f8d74c5ca8d040b3306778b67c87c806
IT86LDR\ahmed.kabbash <ahmedkabbash@gmail.com>
2019-01-27 11:20:03 +0200

    change env param

--- Trainer-FrontEnd/src/app/articles/articles.service.ts
+++ Trainer-FrontEnd/src/app/articles/articles.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { ActivatedRouteSnapshot } from '@angular/router';
-import { ArticleDetialsDto } from 'src/app/shared/models/article-details-dto';
 
 @Injectable()
 export class ArticlesService {
