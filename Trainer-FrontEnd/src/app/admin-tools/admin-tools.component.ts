9c192f293eabcfade3f4fc1743a8e87e0bc5750b
mustafa ahmed <mustafa.ahmed@algorithmz.local>
2019-01-28 00:43:06 +0200

    centering-loader-and-admin-ui-fixing-and-toggle-dashboard-menu

--- Trainer-FrontEnd/src/app/admin-tools/admin-tools.component.ts
+++ Trainer-FrontEnd/src/app/admin-tools/admin-tools.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-tools.component.css']
 })
 export class AdminToolsComponent implements OnInit {
-
+  closeMenu = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleMenu(close) {
+    if (close) {
+      this.closeMenu = true;
+    } else {
+      this.closeMenu = false;
+    }
+  }
 }
