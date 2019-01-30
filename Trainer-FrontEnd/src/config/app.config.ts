93c87fb903c3863291aaa5c10b4e17e9c91d02bc
IT86LDR\ahmed.kabbash <ahmedkabbash@gmail.com>
2019-01-30 21:05:59 +0200

    ts config

--- Trainer-FrontEnd/src/config/app.config.ts
+++ Trainer-FrontEnd/src/config/app.config.ts
@@ -2,13 +2,12 @@ import { Injectable } from "@angular/core";
 import { IAppConfig } from "./app-config.model";
 import { HttpClient } from "@angular/common/http";
 
-
 @Injectable()
 export class AppConfig {
     static settings: IAppConfig;
     constructor(private http: HttpClient) {}
     load() {
-        const jsonFile = `assets/config/config.json`;
+        const jsonFile = `../assets/config/config.json`;
         return new Promise<void>((resolve, reject) => {
             this.http.get(jsonFile).toPromise().then((response : IAppConfig) => {
                AppConfig.settings = <IAppConfig>response;
