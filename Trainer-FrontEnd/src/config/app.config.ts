28c2559194ce91091194933ce6e51e65ec5a9ca9
IT86LDR\ahmed.kabbash <ahmedkabbash@gmail.com>
2019-02-05 01:05:33 +0200

    fix config.json issue

--- Trainer-FrontEnd/src/config/app.config.ts
+++ Trainer-FrontEnd/src/config/app.config.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@angular/core";
-import { IAppConfig } from "./app-config.model";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { IAppConfig } from './app-config.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 
 @Injectable()
@@ -8,7 +9,7 @@ export class AppConfig {
     static settings: IAppConfig;
     constructor(private http: HttpClient) {}
     load() {
-        const jsonFile = `assets/config/config.json`;
+        const jsonFile = `${environment.frontEndBaseUrl}/assets/config/config.json`;
         return new Promise<void>((resolve, reject) => {
             this.http.get(jsonFile).toPromise().then((response : IAppConfig) => {
                AppConfig.settings = <IAppConfig>response;
