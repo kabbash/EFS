24e64c2888288d8ee444655134dfcc9ac0df7018
ahmed <ahmedkabbash@gmail.com>
2019-02-10 18:30:45 +0200

    change attachement

--- Trainer/AttachmentsManager/Services/AttachmentsManager.cs
+++ Trainer/AttachmentsManager/Services/AttachmentsManager.cs
@@ -40,7 +40,7 @@ namespace Attachments.Core.Services
 
                 var attachmentPath = Path.Combine(GetAttachmentTypePath(fileDto.attachmentType), fileDto.SubFolderName ?? "");
                 var relativeFilePath = Path.Combine(attachmentPath, fileName);
-                relativeFilePath = getRelativeURL(relativeFilePath);
+                // relativeFilePath = getRelativeURL(relativeFilePath);
                // Directory.CreateDirectory(Path.Combine(rootPath, attachmentPath));
                 var fileDestinationPath = Path.Combine(rootPath, relativeFilePath);
 
