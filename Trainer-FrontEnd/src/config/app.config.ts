7a92412a8ffe207b497db87ea397065aa57739b8
kabbash <ahmedkabbash@gmail.com>
2019-02-14 13:33:15 +0200

    Update app.config.ts

--- Trainer-FrontEnd/src/config/app.config.ts
+++ Trainer-FrontEnd/src/config/app.config.ts
@@ -9,7 +9,7 @@ export class AppConfig {
     static settings: IAppConfig;
     constructor(private http: HttpClient) {}
     load() {
-        const jsonFile = `/assets/config/config.json`;
+        const jsonFile = `${environment.frontEndBaseUrl}/assets/config/config.json`;
         return new Promise<void>((resolve, reject) => {
             this.http.get(jsonFile).toPromise().then((response : IAppConfig) => {
                AppConfig.settings = <IAppConfig>response;
