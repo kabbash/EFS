5c0a5f73088d72a829c4da056b33a83efe2bc29b
ElHussein Ali <marwa.elhusseinali@vodafone.com>
2019-04-18 10:28:00 +0200

    online training packages

--- Trainer-FrontEnd/src/app/online-training/online-training.module.ts
+++ Trainer-FrontEnd/src/app/online-training/online-training.module.ts
@@ -3,7 +3,7 @@ import { SharedModule } from '../shared/shared.module';
 import { NgbModule, NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { OnlineTrainingComponent } from './online-training.component';
 import { OnlineTrainingRoutingModule } from './online-training-routing.module';
-import { PackagesComponent } from './packages/packages.component';
+import { PackagesComponent } from './training-packages/packages.component';
 
 @NgModule({
   imports: [
