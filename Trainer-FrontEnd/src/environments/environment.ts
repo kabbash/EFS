31b4eb0f183426ef5eb8c1c214f634525ef55275
kabbash <ahmed.kabbash2@vodafone.com>
2019-05-02 21:27:29 +0200

    change env param

--- Trainer-FrontEnd/src/environments/environment.ts
+++ Trainer-FrontEnd/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'http://ec2-34-208-205-223.us-west-2.compute.amazonaws.com:4400/api/',
   filesBaseUrl : 'http://ec2-34-208-205-223.us-west-2.compute.amazonaws.com:4400/',
-  frontEndBaseUrl: 'http://ec2-34-208-205-223.us-west-2.compute.amazonaws.com'
+  frontEndBaseUrl: 'https://egyfitstore.com'
 };
