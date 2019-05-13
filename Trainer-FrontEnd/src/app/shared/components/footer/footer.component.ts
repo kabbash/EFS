377b25807770af430ad3209d480346ffd8202365
mustafa-ahmed <mustafa20060848@gmail.com>
2019-05-13 23:41:55 +0300

    save changes

--- Trainer-FrontEnd/src/app/shared/components/footer/footer.component.ts
+++ Trainer-FrontEnd/src/app/shared/components/footer/footer.component.ts
@@ -7,16 +7,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  footerLogo = false;
+  // footerLogo = false;
   constructor(private router: Router) {
-    router.events.subscribe((val: any) => {
-      // see also
-      if (val && val.route && val.route.path && val.route.path === 'home') {
-        this.footerLogo = true;
-      } else if (val && val.route && val.route.path && val.route.path !== 'home') {
-        this.footerLogo = false;
-      }
-    });
+    // router.events.subscribe((val: any) => {
+    //   // see also
+    //   if (val && val.route && val.route.path && val.route.path === 'home') {
+    //     this.footerLogo = true;
+    //   } else if (val && val.route && val.route.path && val.route.path !== 'home') {
+    //     this.footerLogo = false;
+    //   }
+    // });
   }
 
   ngOnInit() {
