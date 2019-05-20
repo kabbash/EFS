9fff4b81b9c653aa67e0333c6953ce6a64d4b062
ahmed <ahmedkabbash@gmail.com>
2019-05-21 00:50:16 +0200

    implement swagger

--- Trainer/Trainer/Startup.cs
+++ Trainer/Trainer/Startup.cs
@@ -61,6 +61,8 @@ using System.Linq;
 using System.Text;
 using Trainer.EF;
 using DBModels = Shared.Core.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
 
 namespace Trainer
 {
@@ -94,6 +96,23 @@ namespace Trainer
             {
                 options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Info { Title = "You api title", Version = "v1" });
+                c.AddSecurityDefinition("Bearer",
+                    new ApiKeyScheme
+                    {
+                        In = "header",
+                        Description = "Please enter into field the word 'Bearer' following by space and JWT",
+                        Name = "Authorization",
+                        Type = "apiKey"
+                    });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
+                { "Bearer", Enumerable.Empty<string>() },
+            });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -115,6 +134,16 @@ namespace Trainer
             app.UseStaticFiles();
             app.UseCors("AllowAllOrigins");
             app.UseAuthentication();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "efs API V1");
+            });
+
             app.UseMvc();
         }
 
